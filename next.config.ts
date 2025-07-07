6b4892cffd726165e898cd8266135bb05959825a
Abubakar Awan <malikabubkrawan@gmail.com>
2025-07-07 16:53:42 +0500

    fix: added env support and fallback config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import { NextConfigComplete } from "next/dist/server/config-shared";
 
 const nextConfig: NextConfig & Partial<NextConfigComplete> = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve = {
