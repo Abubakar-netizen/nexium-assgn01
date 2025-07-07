3945e51a2e369c187f466046863f0cedbfb238d4
Abubakar Awan <malikabubkrawan@gmail.com>
2025-07-07 16:43:51 +0500

    fix: added Webpack fallbacks for face-api warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import { NextConfigComplete } from "next/dist/server/config-shared";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig & Partial<NextConfigComplete> = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...(config.resolve?.fallback || {}),
+          fs: false,
+          path: false,
+          os: false,
+          encoding: false,
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
